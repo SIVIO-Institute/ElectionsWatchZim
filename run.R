8b962dd9df270c5fa730a92229908a38bace2275
SIVIO-Institute <info@sivioinstitute.org>
2023-07-20 16:53:00 +0200

    20 July update

--- run.R
+++ run.R
@@ -4,14 +4,13 @@ library(git2r)
 repo <- repository("C:/Users/Rebekah Cross/OneDrive - SIVIO Institute/ElectionsWatchZim")
 
 ## Get HEAD of repository
-repository_head(repo)
+## repository_head(repo)
 
 config(repo, user.name="SIVIO-Institute", user.email="info@sivioinstitute.org", user.password="S1vio2022!")
 
 ## Display configuration
-config(repo)
-cred <- cred_env("SIVIO-Institute",
-                 "S1vio2022!")
+##config(repo)
+##cred <- cred_env("SIVIO-Institute","S1vio2022!")
 
 ## Add file and commit
 add(repo, "summary.json")
